769cf8532cd9528ea704541a159cf12c95091b12
Vincent Gurtler <vincent.guertler@gmail.com>
2018-09-26 20:27:25 +0200

    Add the test class for the MenuActivity

---
+++ app/src/androidTest/java/ch/epfl/sweng/zuluzulu/MenuActivityTest.java
@@ -0,0 +1,16 @@
+package ch.epfl.sweng.zuluzulu;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class MenuActivityTest {
+    @Rule
+    public final ActivityTestRule<MenuActivity> mActivityRule =
+            new ActivityTestRule<>(MenuActivity.class);
+    @Test
+    public void testSomething() {
+        // Todo
+    }
+}
