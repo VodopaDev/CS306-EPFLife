49c7dcf92884d13efab7da5a35324762625d7c9f
Vincent Gurtler <vincent.guertler@gmail.com>
2018-09-30 13:32:35 +0200

    #2 Add a test to open the navigation view

--- app/src/androidTest/java/ch/epfl/sweng/zuluzulu/MenuActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/zuluzulu/MenuActivityTest.java
@@ -1,9 +1,16 @@
 package ch.epfl.sweng.zuluzulu;
 
+import android.support.test.espresso.contrib.DrawerActions;
 import android.support.test.rule.ActivityTestRule;
+import android.view.Gravity;
 
 import org.junit.Rule;
 import org.junit.Test;
+
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.DrawerMatchers.isClosed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.Espresso.onView;
 
 public class MenuActivityTest {
     @Rule
@@ -11,6 +18,9 @@ public class MenuActivityTest {
             new ActivityTestRule<>(MenuActivity.class);
     @Test
     public void testSomething() {
-        // Todo
+        // Open Drawer to click on navigation.
+        onView(withId(R.id.drawer_layout))
+                .check(matches(isClosed(Gravity.LEFT))) // Left Drawer should be closed.
+                .perform(DrawerActions.open()); // Open Drawer
     }
 }
