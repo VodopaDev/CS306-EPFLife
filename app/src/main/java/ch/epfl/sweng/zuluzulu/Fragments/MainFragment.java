13f72c53a6d5ff06329bedb1187e964e1fadc764
Vincent Gurtler <vincent.guertler@gmail.com>
2018-10-01 13:36:33 +0200

    #2 Add a button to test the fragment's behavior

--- app/src/main/java/ch/epfl/sweng/zuluzulu/Fragments/MainFragment.java
+++ app/src/main/java/ch/epfl/sweng/zuluzulu/Fragments/MainFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import ch.epfl.sweng.zuluzulu.R;
 
@@ -23,6 +24,8 @@ public class MainFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    private int nbClicks;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -65,7 +68,19 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        nbClicks = 0;
+        Button testButton = view.findViewById(R.id.main_fragment_test_button);
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nbClicks++;
+                System.out.println("click: " + nbClicks);
+            }
+        });
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
