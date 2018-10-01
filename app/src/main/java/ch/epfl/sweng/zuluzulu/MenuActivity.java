03537d1f69e9bd162a7916d46e5d8b832e0694c5
Vincent Gurtler <vincent.guertler@gmail.com>
2018-10-01 10:22:23 +0200

    #2 Change the navigation view to handle fragments

--- app/src/main/java/ch/epfl/sweng/zuluzulu/MenuActivity.java
+++ app/src/main/java/ch/epfl/sweng/zuluzulu/MenuActivity.java
@@ -1,16 +1,21 @@
 package ch.epfl.sweng.zuluzulu;
 
+import android.net.Uri;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-public class MenuActivity extends AppCompatActivity {
+import ch.epfl.sweng.zuluzulu.Fragments.AboutZuluzuluFragment;
+
+public class MenuActivity extends AppCompatActivity implements AboutZuluzuluFragment.OnFragmentInteractionListener {
 
     private DrawerLayout drawerLayout;
 
@@ -20,30 +25,9 @@ public class MenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
 
         drawerLayout = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        Toolbar toolbar = findViewById(R.id.toolbar);
 
-        setSupportActionBar(toolbar);
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
-
-        navigationView.setNavigationItemSelectedListener(
-                new NavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                        // set item as selected to persist highlight
-                        menuItem.setChecked(true);
-                        // close drawer when item is tapped
-                        drawerLayout.closeDrawers();
-
-                        // Add code here to update the UI based on the item selected
-                        // For example, swap UI fragments here
-
-                        return true;
-                    }
-                }
-        );
+        NavigationView navigationView = initNavigationView();
+        initDrawerContent(navigationView);
     }
 
     @Override
@@ -54,5 +38,59 @@ public class MenuActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private NavigationView initNavigationView() {
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        return navigationView;
+    }
+
+    private void initDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        selectItem(menuItem);
+                        return true;
+                    }
+                }
+        );
+    }
+
+    private void selectItem(MenuItem menuItem) {
+        Fragment fragment = null;
+        Class fragmentClass;
+        switch (menuItem.getItemId()) {
+            case R.id.nav_about:
+                fragmentClass = AboutZuluzuluFragment.class;
+                break;
+            default:
+                fragmentClass = AboutZuluzuluFragment.class;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.fragmentContent, fragment).commit();
+
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        drawerLayout.closeDrawers();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
