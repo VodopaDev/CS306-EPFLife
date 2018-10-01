0f39a739414cdc2d9807d77f998f4bcffccea081
Dahn Y <dahn.youssefi@epfl.ch>
2018-10-01 21:09:42 +0200

    RUN OPTIMIZE IMPORTS

--- app/src/main/java/ch/epfl/sweng/zuluzulu/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/zuluzulu/MainActivity.java
@@ -1,11 +1,8 @@
 package ch.epfl.sweng.zuluzulu;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.net.Uri;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -13,8 +10,11 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import ch.epfl.sweng.zuluzulu.Fragments.AboutZuluzuluFragment;
 import ch.epfl.sweng.zuluzulu.Fragments.MainFragment;
