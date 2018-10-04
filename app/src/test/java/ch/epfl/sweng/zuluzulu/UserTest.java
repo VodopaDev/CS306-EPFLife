d03dce137ec202f7613b5d3dce68a865e7f02bb1
Dahn Y <dahn.youssefi@epfl.ch>
2018-10-04 17:02:48 +0200

    ADD TESTS #15

--- app/src/test/java/ch/epfl/sweng/zuluzulu/UserTest.java
+++ app/src/test/java/ch/epfl/sweng/zuluzulu/UserTest.java
@@ -5,9 +5,11 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import ch.epfl.sweng.zuluzulu.Structure.AuthenticatedUser;
+import ch.epfl.sweng.zuluzulu.Structure.Guest;
 import ch.epfl.sweng.zuluzulu.Structure.User;
 
 
@@ -32,4 +34,39 @@ public class UserTest {
         assertEquals(user.getFirst_names(),"first_name");
         assertEquals(user.getLast_names(),"last_name");
     }
+
+    @Test
+    public void canCreateGuestUser() {
+        User.UserBuilder builder = new User.UserBuilder();
+        builder.setLast_names("last_name");
+
+        User user = builder.build();
+
+        Guest user2 = builder.buildGuestUser();
+        assertNotNull(user2);
+
+        assertTrue(user instanceof Guest);
+
+        assertEquals(user.getEmail(),null);
+        assertEquals(user.getSciper(),null);
+        assertEquals(user.getGaspar(),null);
+        assertEquals(user.getFirst_names(),null);
+        assertEquals(user.getLast_names(),null);
+    }
+
+
+    @Test
+    public void refuseWrongMail() {
+        User.UserBuilder builder = new User.UserBuilder();
+        builder.setEmail("fakemail");
+        builder.setSciper("1212");
+        builder.setGaspar("test");
+        builder.setFirst_names("first_name");
+        builder.setLast_names("last_name");
+
+        User user = builder.build();
+
+        assertTrue(user instanceof Guest);
+    }
+
 }
