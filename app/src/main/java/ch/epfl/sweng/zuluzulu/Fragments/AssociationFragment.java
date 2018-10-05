713d440afa4d25d697577afc542f60ce2e756d6e
Nicolas Jomeau <nicolas.jomeau@outlook.com>
2018-10-05 11:58:34 +0200

    #17 Added layout and fragment

---
+++ app/src/main/java/ch/epfl/sweng/zuluzulu/Fragments/AssociationFragment.java
@@ -0,0 +1,93 @@
+package ch.epfl.sweng.zuluzulu.Fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ScrollView;
+
+import ch.epfl.sweng.zuluzulu.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link AssociationFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link AssociationFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AssociationFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+    private Button button_all;
+    private Button button_fav;
+    private ScrollView scroll;
+
+    public AssociationFragment() {
+        // Required empty public constructor
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public static AssociationFragment newInstance(String param1, String param2) {
+        AssociationFragment fragment = new AssociationFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_association, container, false);
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
