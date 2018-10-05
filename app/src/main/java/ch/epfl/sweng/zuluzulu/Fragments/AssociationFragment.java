430172b5898c28f29479fd93c15c2ff974805013
Nicolas Jomeau <nicolas.jomeau@outlook.com>
2018-10-05 12:15:41 +0200

    #17 Functionnal scroll view

--- app/src/main/java/ch/epfl/sweng/zuluzulu/Fragments/AssociationFragment.java
+++ app/src/main/java/ch/epfl/sweng/zuluzulu/Fragments/AssociationFragment.java
@@ -8,24 +8,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.TextView;
 
+import ch.epfl.sweng.zuluzulu.OnFragmentInteractionListener;
 import ch.epfl.sweng.zuluzulu.R;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link AssociationFragment.OnFragmentInteractionListener} interface
+ * {@link OnFragmentInteractionListener} interface
  * to handle interaction events.
  * Use the {@link AssociationFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class AssociationFragment extends Fragment {
+    public static final String TAG = "ASSOCIATION_TAG";
 
     private OnFragmentInteractionListener mListener;
     private Button button_all;
     private Button button_fav;
-    private ScrollView scroll;
+    private LinearLayout buttons_layout;
 
     public AssociationFragment() {
         // Required empty public constructor
@@ -49,13 +53,21 @@ public class AssociationFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_association, container, false);
+        buttons_layout = view.findViewById(R.id.assos_layout);
+
+        for(int i = 0; i < 100; i++){
+            TextView text = new TextView(getContext());
+            text.setText("text test" + i);
+            buttons_layout.addView(text);
+        }
+
         return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+            mListener.onFragmentInteraction(TAG, uri);
         }
     }
 
@@ -76,18 +88,4 @@ public class AssociationFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
