149530b0e6e8198a281c6cc1fdd5e5c181d66703
Nicolas Jomeau <nicolas.jomeau@outlook.com>
2018-10-07 19:27:15 +0200

    Changed tests

--- app/src/test/java/ch/epfl/sweng/zuluzulu/AssociationTest.java
+++ app/src/test/java/ch/epfl/sweng/zuluzulu/AssociationTest.java
@@ -1,15 +1,49 @@
 package ch.epfl.sweng.zuluzulu;
 
-import org.junit.Before;
+import android.content.Context;
+import android.content.pm.InstrumentationInfo;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.junit.Before;
+import org.junit.Test;
 import ch.epfl.sweng.zuluzulu.Structure.Association;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class AssociationTest {
+    private static final String NAME = "Agepoly";
+    private static final String SHORT_DESC = "Representing all students at EPFL";
+    private static final String LONG_DESC = "Blah blah blah blah blah";
 
     private Association asso;
 
     @Before
     public void createAssociation(){
-
+        FirebaseApp.initializeApp();
+        asso = Association.fromId(1);
+        while(!asso.hasLoaded()){}
     }
+
+    @Test
+    public void nameIsCorrect(){
+        assertEquals(NAME, asso.getName());
+    }
+
+    @Test
+    public void longDescIsCorrect(){
+        assertEquals(LONG_DESC, asso.getLongDesc());
+    }
+
+    @Test
+    public void shortDescIsCorrect(){
+        assertEquals(SHORT_DESC, asso.getShortDesc());
+    }
+
+    @Test
+    public void uriIsCorrect(){
+        assertNotNull(asso.getIcon());
+    }
+
 }
