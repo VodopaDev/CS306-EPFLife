56abd0aa24d26b881dc93b7728a58f2eb28f7681
Dahn Y <dahn.youssefi@epfl.ch>
2018-10-09 14:28:22 +0200

    TESTS FINISHED #19

--- app/src/androidTest/java/ch/epfl/sweng/zuluzulu/LogoutTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/zuluzulu/LogoutTest.java
@@ -1,11 +1,16 @@
 package ch.epfl.sweng.zuluzulu;
 
+
+import android.support.design.widget.NavigationView;
 import android.support.test.espresso.contrib.DrawerActions;
 import android.support.test.espresso.contrib.NavigationViewActions;
+import android.support.test.espresso.matcher.BoundedMatcher;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.view.Gravity;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -15,22 +20,14 @@ import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.contrib.DrawerMatchers.isClosed;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
 public class LogoutTest {
+    @Rule
+    public final ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
 
-    private void openLoginFragment() {
-        // Open Drawer to click on navigation.
-        onView(withId(R.id.drawer_layout))
-                .check(matches(isClosed(Gravity.LEFT))) // Left Drawer should be closed.
-                .perform(DrawerActions.open()); // Open Drawer
-
-        // Click on the login item in the Drawer
-        onView(withId(R.id.nav_view))
-                .perform(NavigationViewActions.navigateTo(R.id.nav_login));
-    }
 
     @Before
     public void logIn() {
@@ -44,10 +41,28 @@ public class LogoutTest {
 
     @Test
     public void Logout() {
+        openDrawer();
         onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_logout));
 
-        onView(withId(R.id.nav_login)).check(matches(isDisplayed()));
+        openDrawer();
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_login));
     }
 
+
+    private void openDrawer(){
+        // Open Drawer to click on navigation.
+        onView(withId(R.id.drawer_layout))
+                .check(matches(isClosed(Gravity.LEFT))) // Left Drawer should be closed.
+                .perform(DrawerActions.open()); // Open Drawer
+
+    }
+
+    private void openLoginFragment() {
+        openDrawer();
+
+        // Click on the login item in the Drawer
+        onView(withId(R.id.nav_view))
+                .perform(NavigationViewActions.navigateTo(R.id.nav_login));
+    }
 
 }
