37ce056119e89466f58d8ba41934136ec9425496
Vincent Gurtler <vincent.guertler@gmail.com>
2018-10-11 13:49:33 +0200

    #36 Set the root view not final

--- app/src/main/java/ch/epfl/sweng/zuluzulu/Fragments/ChatFragment.java
+++ app/src/main/java/ch/epfl/sweng/zuluzulu/Fragments/ChatFragment.java
@@ -83,7 +83,7 @@ public class ChatFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        final View view = inflater.inflate(R.layout.fragment_chat, container, false);
+        View view = inflater.inflate(R.layout.fragment_chat, container, false);
 
         sendButton = view.findViewById(R.id.chat_send_button);
         textEdit = view.findViewById(R.id.chat_message);
