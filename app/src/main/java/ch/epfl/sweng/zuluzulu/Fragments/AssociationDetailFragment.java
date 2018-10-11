f50daec43fad9c04e8af74920782cb59e1dff124
Nicolas Jomeau <nicolas.jomeau@epfl.ch>
2018-10-11 13:40:38 +0200

    #34 Fixed hardcoded string in association_card

--- app/src/main/java/ch/epfl/sweng/zuluzulu/Fragments/AssociationDetailFragment.java
+++ app/src/main/java/ch/epfl/sweng/zuluzulu/Fragments/AssociationDetailFragment.java
@@ -1,9 +1,14 @@
 package ch.epfl.sweng.zuluzulu.Fragments;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import ch.epfl.sweng.zuluzulu.OnFragmentInteractionListener;
+import ch.epfl.sweng.zuluzulu.R;
 import ch.epfl.sweng.zuluzulu.Structure.Association;
 import ch.epfl.sweng.zuluzulu.Structure.User;
 
@@ -25,6 +30,12 @@ public class AssociationDetailFragment extends Fragment{
         fragment.setUser(user);
         fragment.setAssociation(association);
         return fragment;
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_association, container, false);
+        return view;
     }
 
     private void setUser(User user){
