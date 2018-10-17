7f7999f4936cd9510ed731910fcf375ddb569472
Vincent Gurtler <vincent.guertler@gmail.com>
2018-10-17 15:12:44 +0200

    #72 The message layout is differente if its your own message

--- app/src/main/java/ch/epfl/sweng/zuluzulu/Adapters/ChatMessageAdapter.java
+++ app/src/main/java/ch/epfl/sweng/zuluzulu/Adapters/ChatMessageAdapter.java
@@ -1,6 +1,7 @@
 package ch.epfl.sweng.zuluzulu.Adapters;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -34,11 +35,19 @@ public class ChatMessageAdapter extends ArrayAdapter<ChatMessage> {
         }
 
         ChatMessage currentChatMessage = messages.get(position);
-
-        TextView senderName = messageView.findViewById(R.id.chat_message_senderName);
-        senderName.setText(currentChatMessage.getSenderName());
+        boolean isOwnMessage = currentChatMessage.isOwnMessage();
 
         TextView message = messageView.findViewById(R.id.chat_message_msg);
+        TextView senderName = messageView.findViewById(R.id.chat_message_senderName);
+
+        if (isOwnMessage) {
+            senderName.setText("You");
+            senderName.setBackgroundColor(Color.GREEN);
+        }
+        else {
+            senderName.setText(currentChatMessage.getSenderName());
+        }
+
         message.setText(currentChatMessage.getMessage());
 
         return messageView;
