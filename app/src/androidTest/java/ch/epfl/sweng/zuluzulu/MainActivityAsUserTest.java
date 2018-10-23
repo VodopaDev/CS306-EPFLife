a370a893a561f401340e88e9823c7b75f1241c1d
Gaultier Lonfat <gaultier.lonfat@epfl.ch>
2018-10-23 19:36:14 +0200

    #80 new main fragment for user with logo, ready for merging

--- app/src/androidTest/java/ch/epfl/sweng/zuluzulu/MainActivityAsUserTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/zuluzulu/MainActivityAsUserTest.java
@@ -2,6 +2,10 @@ package ch.epfl.sweng.zuluzulu;
 
 import org.junit.Test;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -13,6 +17,6 @@ public class MainActivityAsUserTest extends TestWithLogin {
     @Test
     public void isAuthenticated() {
         // check not authenticated
-        assertTrue(mActivityRule.getActivity().isAuthenticated());
+        onView(withId(R.id.main_user_fragment)).check(matches(isDisplayed()));
     }
 }
