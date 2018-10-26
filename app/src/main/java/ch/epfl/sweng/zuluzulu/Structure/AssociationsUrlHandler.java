1229d7f4c38eadd5d728eac87a923e91ec595d3b
Dahn Y <dahn.youssefi@epfl.ch>
2018-10-26 21:13:10 +0200

    REFACTOR AssociationsUrlHandler #77

--- app/src/main/java/ch/epfl/sweng/zuluzulu/Structure/AssociationsUrlHandler.java
+++ app/src/main/java/ch/epfl/sweng/zuluzulu/Structure/AssociationsUrlHandler.java
@@ -1,8 +1,6 @@
 package ch.epfl.sweng.zuluzulu.Structure;
 
 import android.os.AsyncTask;
-import android.text.Html;
-import android.util.Pair;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,6 +9,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -27,62 +26,63 @@ public class AssociationsUrlHandler extends AsyncTask<String, Void, Long> {
 
     @Override
     protected Long doInBackground(String... urls) {
-        System.out.println("TEST");
-        connect(urls[0]);
+
+        ArrayList<String> result = parseUrl(urls[0]);
+
+
         return null;
     }
 
 
-    public boolean connect(String url) {
-        URL aURL = null;
-        HttpURLConnection UrlConnection;
-        int code = 0;
+    private ArrayList<String> parseUrl(String url) {
+
+        HttpURLConnection urlConnection = null;
         try {
-            // Open url
-            aURL = openUrl(url);
-
-            // Open connection
-            UrlConnection = (HttpURLConnection) aURL.openConnection();
-            UrlConnection.connect();
-
-            // Get HTTP response code
-            code = UrlConnection.getResponseCode();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-            return false;
+            urlConnection = connect(url);
         } catch (IOException e) {
             e.printStackTrace();
-            return false;
+            return null;
         }
 
-        if(code != 200){
-            // Not OK response
-            UrlConnection.disconnect();
-            return false;
+        if(urlConnection == null){
+            return null;
         }
 
-        System.out.println("CONNECTED !");
-
-
+        ArrayList<String> datas = null;
         try {
-            // Parse the datas
-            parseData(UrlConnection.getInputStream());
+            datas = parseData(urlConnection.getInputStream());
         } catch (IOException e) {
             e.printStackTrace();
-            UrlConnection.disconnect();
-            return false;
+        } finally {
+            urlConnection.disconnect();
         }
 
-
-        UrlConnection.disconnect();
-
-
-
-
-        return true;
+        return datas;
     }
 
-    private void parseData(InputStream inputStream) throws IOException {
+    private HttpURLConnection connect(String url) throws IOException {
+        // Open url
+        URL aURL = openUrl(url);
+
+        // Open connection
+        HttpURLConnection UrlConnection = (HttpURLConnection) aURL.openConnection();
+        UrlConnection.connect();
+
+        // Get HTTP response code
+        int code = UrlConnection.getResponseCode();
+
+
+        if (code != 200) {
+            // Not OK response
+            UrlConnection.disconnect();
+            return null;
+        }
+
+        return UrlConnection;
+    }
+
+
+    private ArrayList<String> parseData(InputStream inputStream) throws IOException {
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(inputStream));
 
@@ -96,12 +96,13 @@ public class AssociationsUrlHandler extends AsyncTask<String, Void, Long> {
         while ((inputLine = in.readLine()) != null) {
             Matcher m = p.matcher(inputLine);
             if (m.find()) {
-                System.out.println("Name : " + m.group(2));
                 // remove the span tag
                 String description = m.group(3).replaceAll("<.*>", "");
+
+
+                System.out.println("Name : " + m.group(2));
                 System.out.println("D3 : " + description);
                 System.out.println("-----------");
-
 
                 StringBuilder sb = new StringBuilder();
 
@@ -115,6 +116,8 @@ public class AssociationsUrlHandler extends AsyncTask<String, Void, Long> {
             }
         }
         in.close();
+
+        return results;
     }
 
     private URL openUrl(String url) throws MalformedURLException {
