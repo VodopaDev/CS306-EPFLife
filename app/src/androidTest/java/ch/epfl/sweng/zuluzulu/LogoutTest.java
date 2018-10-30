d10232ecd4cc589e348a7d7f491c1a54ca268068
Dahn Y <dahn.youssefi@epfl.ch>
2018-10-30 11:45:16 +0100

    ADD LOGOUT TEST

---
+++ app/src/androidTest/java/ch/epfl/sweng/zuluzulu/LogoutTest.java
@@ -0,0 +1,11 @@
+package ch.epfl.sweng.zuluzulu;
+
+import org.junit.Test;
+
+public class LogoutTest extends TestWithLogin {
+    @Test
+    public void logout(){
+        Utility.openMenu();
+        
+    }
+}
