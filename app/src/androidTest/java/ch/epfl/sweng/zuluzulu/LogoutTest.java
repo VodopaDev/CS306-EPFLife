ccb377e9aa6e6fedd47b562dd20b6f619527024f
Dahn Y <dahn.youssefi@epfl.ch>
2018-10-30 11:46:49 +0100

    Logout test added

--- app/src/androidTest/java/ch/epfl/sweng/zuluzulu/LogoutTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/zuluzulu/LogoutTest.java
@@ -1,11 +1,19 @@
 package ch.epfl.sweng.zuluzulu;
 
+import android.support.test.espresso.contrib.NavigationViewActions;
+
 import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 public class LogoutTest extends TestWithLogin {
     @Test
-    public void logout(){
+    public void logout() {
         Utility.openMenu();
-        
+
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_logout));
+
+        Utility.checkFragmentIsOpen(R.id.main_fragment);
     }
 }
