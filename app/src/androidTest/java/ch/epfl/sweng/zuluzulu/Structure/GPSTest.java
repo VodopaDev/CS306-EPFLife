411de25b320190b90530082c4c9f68bbd756039f
Dahn Y <dahn.youssefi@epfl.ch>
2018-10-30 22:08:08 +0100

    Grant GPS in tests

--- app/src/androidTest/java/ch/epfl/sweng/zuluzulu/Structure/GPSTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/zuluzulu/Structure/GPSTest.java
@@ -3,6 +3,7 @@ package ch.epfl.sweng.zuluzulu.Structure;
 import android.location.Location;
 import android.location.LocationManager;
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.rule.GrantPermissionRule;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -13,6 +14,9 @@ public class GPSTest {
     @Rule
     public final ActivityTestRule<MainActivity> mActivityRule =
             new ActivityTestRule<>(MainActivity.class);
+
+    @Rule public GrantPermissionRule permissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION);
+
 
     @Test
     public void testOnLocationChanged() throws Throwable {
