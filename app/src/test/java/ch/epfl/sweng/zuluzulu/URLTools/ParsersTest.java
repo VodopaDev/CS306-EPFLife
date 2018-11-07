6ba2ca79404a9c0446968964d6db7b19811196c7
Dahn Y <dahn.youssefi@epfl.ch>
2018-11-07 09:57:19 +0100

    Add tests to parsers #135

--- app/src/test/java/ch/epfl/sweng/zuluzulu/URLTools/ParsersTest.java
+++ app/src/test/java/ch/epfl/sweng/zuluzulu/URLTools/ParsersTest.java
@@ -3,10 +3,7 @@ package ch.epfl.sweng.zuluzulu.URLTools;
 import org.junit.Test;
 
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.StringReader;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
