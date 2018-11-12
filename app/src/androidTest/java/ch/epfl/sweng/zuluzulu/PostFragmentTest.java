938a744a6031b42132bcce923a22be6e829ae9f1
Vincent Gurtler <vincent.guertler@gmail.com>
2018-11-12 11:16:59 +0100

    #152 Add post fragment tests

--- app/src/androidTest/java/ch/epfl/sweng/zuluzulu/PostFragmentTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/zuluzulu/PostFragmentTest.java
@@ -1,6 +1,5 @@
 package ch.epfl.sweng.zuluzulu;
 
-import android.support.test.espresso.action.ViewActions;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Before;
@@ -11,19 +10,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 import ch.epfl.sweng.zuluzulu.Firebase.FirebaseMapDecorator;
-import ch.epfl.sweng.zuluzulu.Fragments.ChatFragment;
+import ch.epfl.sweng.zuluzulu.Fragments.PostFragment;
 import ch.epfl.sweng.zuluzulu.Fragments.SuperFragment;
 import ch.epfl.sweng.zuluzulu.Structure.Channel;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static org.hamcrest.core.IsNot.not;
 
 @RunWith(AndroidJUnit4.class)
-public class ChatFragmentTest extends TestWithLogin {
+public class PostFragmentTest extends TestWithLogin {
 
     @Before
     public void init() {
@@ -34,24 +31,12 @@ public class ChatFragmentTest extends TestWithLogin {
         data.put("restrictions", new HashMap<>());
         FirebaseMapDecorator fmap = new FirebaseMapDecorator(data);
         Channel channel = new Channel(fmap);
-        SuperFragment fragment = ChatFragment.newInstance(getUser(), channel);
+        SuperFragment fragment = PostFragment.newInstance(getUser(), channel);
         mActivityRule.getActivity().openFragment(fragment);
     }
 
     @Test
-    public void testUserCanSeeMessages() {
-        onView(withId(R.id.chat_list_view)).check(matches(isDisplayed()));
-    }
-
-    @Test
-    public void testUserCanWriteInTextEditBox() {
-        onView(withId(R.id.chat_message_edit)).perform(ViewActions.typeText("test")).perform(ViewActions.closeSoftKeyboard());
-    }
-
-    @Test
-    public void testSendButtonIsEnabledOnlyIfMessageNotEmpty() {
-        onView(withId(R.id.chat_send_button)).check(matches(not(isEnabled())));
-        onView(withId(R.id.chat_message_edit)).perform(ViewActions.typeText("test")).perform(ViewActions.closeSoftKeyboard());
-        onView(withId(R.id.chat_send_button)).check(matches(isEnabled()));
+    public void testUserCanSeePosts() {
+        onView(withId(R.id.posts_list_view)).check(matches(isDisplayed()));
     }
 }
