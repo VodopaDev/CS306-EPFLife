4b9559d38ddf1c41d55a3345e7c516e5f76e9b9c
Dahn Y <dahn.youssefi@epfl.ch>
2018-11-18 12:29:24 +0100

    test

--- app/src/androidTest/java/ch/epfl/sweng/zuluzulu/MementoFragmentTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/zuluzulu/MementoFragmentTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 
 import java.io.BufferedReader;
 import java.io.StringReader;
-import java.util.concurrent.TimeUnit;
 
 import ch.epfl.sweng.zuluzulu.Database.FirebaseMock;
 import ch.epfl.sweng.zuluzulu.Firebase.DatabaseFactory;
