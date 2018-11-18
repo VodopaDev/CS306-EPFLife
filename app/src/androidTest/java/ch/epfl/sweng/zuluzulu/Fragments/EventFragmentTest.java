15800d69606f41211b58e401468a3f5f90e64087
Yann <yann.gabbud@epfl.ch>
2018-11-18 22:32:55 +0100

    #174 update eventFragment tests

--- app/src/androidTest/java/ch/epfl/sweng/zuluzulu/Fragments/EventFragmentTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/zuluzulu/Fragments/EventFragmentTest.java
@@ -1,5 +1,6 @@
 package ch.epfl.sweng.zuluzulu.Fragments;
 
+import android.support.test.espresso.ViewAssertion;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.runner.AndroidJUnit4;
 import android.view.View;
@@ -12,7 +13,6 @@ import org.junit.runner.RunWith;
 import java.util.concurrent.TimeUnit;
 
 import ch.epfl.sweng.zuluzulu.Adapters.EventArrayAdapter;
-import ch.epfl.sweng.zuluzulu.MainActivity;
 import ch.epfl.sweng.zuluzulu.R;
 import ch.epfl.sweng.zuluzulu.TestWithAuthenticatedUser;
 
@@ -24,6 +24,7 @@ import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.hasChildCount;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withChild;
@@ -31,17 +32,22 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
+import static org.hamcrest.Matchers.isEmptyString;
 
 @RunWith(AndroidJUnit4.class)
 public class EventFragmentTest extends TestWithAuthenticatedUser {
     SuperFragment fragment;
 
     @Before
-    public void init() {
+    public void init() throws InterruptedException {
         fragment = EventFragment.newInstance(getUser());
         mActivityRule.getActivity().openFragment(fragment);
+
+        TimeUnit.SECONDS.sleep(3);
     }
 
     @Test
@@ -70,6 +76,12 @@ public class EventFragmentTest extends TestWithAuthenticatedUser {
     }
 
     @Test
+    public void thereIsEventInTheListView(){
+        onView(withId(R.id.event_fragment_listview)).check(matches(hasMinimumChildCount(4)));
+        onView(withId(R.id.event_fragment_listview)).check(matches(hasDescendant(withText("ForumEPFL"))));
+    }
+
+    @Test
     public void sortEventWithTheThreeSimpleSort() throws InterruptedException{
         onView(withId(R.id.event_fragment_checkBox_sort_date)).perform(ViewActions.click());
         onView(withId(R.id.event_fragment_checkbox_sort_like)).perform(ViewActions.click());
@@ -77,46 +89,43 @@ public class EventFragmentTest extends TestWithAuthenticatedUser {
     }
 
     @Test
-    public void sortWithKeyworkTest() {
+    public void sortWithKeywordTest1() throws InterruptedException {
         onView(withId(R.id.event_fragment_search_bar)).perform(typeText("forum"));
-
-        onData(instanceOf(EventFragment.class))
+        onData(anything())
                 .inAdapterView(withId(R.id.event_fragment_listview))
-                .atPosition(1)
-//                .onChildView(withId(R.id.card_event_name))
-                .perform(click());
-
-//        onView(withId(R.id.event_fragment_checkBox_sort_name)).perform(ViewActions.click());
+//                .atPosition(0)
+                .onChildView(withId(R.id.card_event_name))
+                .check(matches(withText("ForumEPFL")));
     }
 
     @Test
-    public void sortFromDate() throws InterruptedException {
+    public void sortWithKeywordTest2(){
+        onView(withId(R.id.event_fragment_search_bar)).perform(typeText("discover"));
+        onData(anything())
+                .inAdapterView(withId(R.id.event_fragment_listview))
+//                .atPosition(0)
+                .onChildView(withId(R.id.card_event_name))
+                .check(matches(withText("ForumEPFL")));
+    }
+
+    @Test
+    public void sortFromDate() {
         onView(withId(R.id.event_fragment_from_date)).perform(click());
         onView(withText("OK")).perform(click());
     }
 
     @Test
-    public void sortToDate(){
-
+    public void sortToDateFail(){
+        onView(withId(R.id.event_fragment_to_date)).perform((click()));
+        onView(withText("OK")).perform(click());
+        onView(withId(R.id.event_fragment_to_date)).check(matches(withText("")));
     }
 
-    @Test
-    public void sortListDateFrom() throws InterruptedException {
-//        TimeUnit.SECONDS.sleep(1);
-        onView(withId(R.id.event_fragment_from_date)).perform(typeText("01012040")).perform(closeSoftKeyboard());
-//        TimeUnit.SECONDS.sleep(1);
-        onView(withId(R.id.event_fragment_checkBox_sort_date)).perform(ViewActions.click());
-//        TimeUnit.SECONDS.sleep(1);
-    }
-
-    @Test
-    public void sortListDateFromAndTo() throws InterruptedException {
-//        TimeUnit.SECONDS.sleep(1);
-        onView(withId(R.id.event_fragment_from_date)).perform(typeText("01012040")).perform(closeSoftKeyboard());
-//        TimeUnit.SECONDS.sleep(1);
-        onView(withId(R.id.event_fragment_to_date)).perform(typeText("01012041")).perform(closeSoftKeyboard());
-//        TimeUnit.SECONDS.sleep(1);
-        onView(withId(R.id.event_fragment_checkBox_sort_date)).perform(ViewActions.click());
-//        TimeUnit.SECONDS.sleep(1);
-    }
+   @Test
+    public void sortFromAndToDate() {
+       onView(withId(R.id.event_fragment_from_date)).perform(click());
+       onView(withText("OK")).perform(click());
+       onView(withId(R.id.event_fragment_to_date)).perform((click()));
+       onView(withText("OK")).perform(click());
+   }
 }
