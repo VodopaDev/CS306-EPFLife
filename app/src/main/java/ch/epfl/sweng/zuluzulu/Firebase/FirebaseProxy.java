8d4ab0f604303b6eea7cfa0a21b756bfaeaa2824
Nicolas Jomeau <nicolas.jomeau@epfl.ch>
2018-11-18 20:07:07 +0100

    Added a method to get all events, associations or channels

--- app/src/main/java/ch/epfl/sweng/zuluzulu/Firebase/FirebaseProxy.java
+++ app/src/main/java/ch/epfl/sweng/zuluzulu/Firebase/FirebaseProxy.java
@@ -1,27 +1,23 @@
-
 package ch.epfl.sweng.zuluzulu.Firebase;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
 import android.util.Log;
 
 import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import ch.epfl.sweng.zuluzulu.CommunicationTag;
 import ch.epfl.sweng.zuluzulu.OnFragmentInteractionListener;
 import ch.epfl.sweng.zuluzulu.Structure.Association;
-import ch.epfl.sweng.zuluzulu.User.AuthenticatedUser;
-import ch.epfl.sweng.zuluzulu.User.User;
+import ch.epfl.sweng.zuluzulu.Structure.Channel;
+import ch.epfl.sweng.zuluzulu.Structure.Event;
+
+import static ch.epfl.sweng.zuluzulu.CommunicationTag.INCREMENT_IDLING_RESOURCE;
+import static ch.epfl.sweng.zuluzulu.CommunicationTag.DECREMENT_IDLING_RESOURCE;
 
 public class FirebaseProxy {
 
@@ -60,15 +56,68 @@ public class FirebaseProxy {
     }
 
     /**
-     *
-     * @param tag Tag of the error message
-     * @param message Body of the error message
-     * @return
+     * Get all associations and apply an OnResult on them
+     * @param onResult interface defining apply()
      */
-    private OnFailureListener failureWithError(String tag, String message) {
+    public void getAllAssociations(OnResult<List<Association>> onResult){
+        mListener.onFragmentInteraction(INCREMENT_IDLING_RESOURCE, null);
+        associationCollection.get().addOnSuccessListener(queryDocumentSnapshots -> {
+            List<Association> resultList = new ArrayList<>();
+            for(DocumentSnapshot snap: queryDocumentSnapshots){
+                FirebaseMapDecorator fmap = new FirebaseMapDecorator(snap);
+                if(fmap.hasFields(Association.FIELDS))
+                    resultList.add(new Association(fmap));
+            }
+            onResult.apply(resultList);
+            mListener.onFragmentInteraction(DECREMENT_IDLING_RESOURCE, null);
+        }).addOnFailureListener(onFailureWithErrorMessage("Cannot fetch all associations"));
+    }
+
+    /**
+     * Get all events and apply an OnResult on them
+     * @param onResult interface defining apply()
+     */
+    public void getAllEvents(OnResult<List<Event>> onResult){
+        mListener.onFragmentInteraction(INCREMENT_IDLING_RESOURCE, null);
+        eventCollection.get().addOnSuccessListener(queryDocumentSnapshots -> {
+            List<Event> resultList = new ArrayList<>();
+            for(DocumentSnapshot snap: queryDocumentSnapshots){
+                FirebaseMapDecorator fmap = new FirebaseMapDecorator(snap);
+                if(fmap.hasFields(Event.FIELDS))
+                    resultList.add(new Event(fmap));
+            }
+            onResult.apply(resultList);
+            mListener.onFragmentInteraction(DECREMENT_IDLING_RESOURCE, null);
+        }).addOnFailureListener(onFailureWithErrorMessage("Cannot fetch all events"));
+    }
+
+    /**
+     * Get all channels and apply an OnResult on them
+     * @param onResult interface defining apply()
+     */
+    public void getAllChannels(OnResult<List<Channel>> onResult){
+        mListener.onFragmentInteraction(INCREMENT_IDLING_RESOURCE, null);
+        channelCollection.get().addOnSuccessListener(queryDocumentSnapshots -> {
+            List<Channel> resultList = new ArrayList<>();
+            for(DocumentSnapshot snap: queryDocumentSnapshots){
+                FirebaseMapDecorator fmap = new FirebaseMapDecorator(snap);
+                if(fmap.hasFields(Channel.FIELDS))
+                    resultList.add(new Channel(fmap));
+            }
+            onResult.apply(resultList);
+            mListener.onFragmentInteraction(DECREMENT_IDLING_RESOURCE, null);
+        }).addOnFailureListener(onFailureWithErrorMessage("Cannot fetch all channels"));
+    }
+
+    /**
+     * Return a new OnFailureListener logging the error in the error section of the console
+     * @param message Body of the error message
+     * @return OnFailureListener with customized text
+     */
+    private OnFailureListener onFailureWithErrorMessage(String message) {
         return e -> {
-            Log.e(tag, message);
-            mListener.onFragmentInteraction(CommunicationTag.DECREMENT_IDLING_RESOURCE, null);
+            Log.e("PROXY", message);
+            mListener.onFragmentInteraction(DECREMENT_IDLING_RESOURCE, null);
         };
     }
 
--- app/src/main/java/ch/epfl/sweng/zuluzulu/Firebase/OnDataFetched.java
+++
@@ -1,5 +0,0 @@
-package ch.epfl.sweng.zuluzulu.Firebase;
-
-public interface OnDataFetched<T> {
-    void apply(T data);
-}
