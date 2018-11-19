48f60bfd549bd6c265914846c8dfaa73ed04d2aa
Vincent Gurtler <vincent.guertler@gmail.com>
2018-11-19 12:40:53 +0100

    #169 Try to test click on up button

--- app/src/androidTest/java/ch/epfl/sweng/zuluzulu/Fragments/PostFragmentTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/zuluzulu/Fragments/PostFragmentTest.java
@@ -8,14 +8,19 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import ch.epfl.sweng.zuluzulu.R;
+import ch.epfl.sweng.zuluzulu.Structure.Post;
 import ch.epfl.sweng.zuluzulu.TestingUtility.TestWithAuthenticatedAndFragment;
 import ch.epfl.sweng.zuluzulu.Utility;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.CoreMatchers.anything;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.IsNot.not;
 
 @RunWith(AndroidJUnit4.class)
@@ -41,5 +46,12 @@ public class PostFragmentTest extends TestWithAuthenticatedAndFragment<PostFragm
     public void testUserCanGoToChat() {
         onView(withId(R.id.chat_button)).perform(ViewActions.click());
         onView(withId(R.id.chat_list_view)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testUserCanUp() {
+        // I don't know how to click on a particular element of one item in the list view
+        // onData(is(instanceOf(Post.class))).atPosition(0).check(matches(isDisplayed()));
+        // onData(anything()).inAdapterView(withId(R.id.posts_list_view)).atPosition(0).check(matches(isDisplayed()));
     }
 }
