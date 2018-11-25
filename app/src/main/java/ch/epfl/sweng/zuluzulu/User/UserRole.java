3a5fe0253abec6d1d3e1243a91c7dc8ce6a8d09a
Nicolas Jomeau <nicolas.jomeau@epfl.ch>
2018-11-25 15:58:41 +0100

    .

--- app/src/main/java/ch/epfl/sweng/zuluzulu/User/UserRole.java
+++ app/src/main/java/ch/epfl/sweng/zuluzulu/User/UserRole.java
@@ -1,9 +1,8 @@
 package ch.epfl.sweng.zuluzulu.User;
 
 /**
- * This enum represent the differents roles for the user
+ * This enum represent the different roles for the user
  */
 public enum UserRole {
     USER, MODERATOR, ADMIN
-
 }
