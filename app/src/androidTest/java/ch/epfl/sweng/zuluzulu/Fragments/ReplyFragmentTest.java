af2585a9b59f0a0ecc1c234bee00caf6bb6786a4
Vincent Gurtler <vincent.guertler@gmail.com>
2018-11-26 15:10:37 +0100

    Add tests on some list view items

--- app/src/androidTest/java/ch/epfl/sweng/zuluzulu/Fragments/ReplyFragmentTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/zuluzulu/Fragments/ReplyFragmentTest.java
@@ -6,17 +6,23 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.TimeUnit;
+
 import ch.epfl.sweng.zuluzulu.Database.FirebaseMock;
 import ch.epfl.sweng.zuluzulu.Firebase.DatabaseFactory;
 import ch.epfl.sweng.zuluzulu.R;
+import ch.epfl.sweng.zuluzulu.Structure.Post;
 import ch.epfl.sweng.zuluzulu.TestingUtility.TestWithAuthenticatedAndFragment;
 import ch.epfl.sweng.zuluzulu.Utility;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.not;
 
 @RunWith(AndroidJUnit4.class)
@@ -51,18 +57,34 @@ public class ReplyFragmentTest extends TestWithAuthenticatedAndFragment<ReplyFra
     public void testUserCanSendReply() {
         onView(withId(R.id.reply_text_edit)).perform(ViewActions.typeText("This is a reply test !")).perform(ViewActions.closeSoftKeyboard());
         onView(withId(R.id.reply_send_button)).check(matches(isEnabled()));
-        onView(withId(R.id.reply_send_button)).perform(ViewActions.click());
+        onView(withId(R.id.reply_send_button)).perform(click());
     }
 
     @Test
-    public void testUserCanUpDownOriginalPost() {
-        // onData(anything()).inAdapterView(withId(R.id.reply_original_post)).atPosition(0).onChildView(withId(R.id.post_up_button)).check(matches(isDisplayed()));
-        // onData(anything()).inAdapterView(withId(R.id.reply_original_post)).atPosition(0).onChildView(withId(R.id.post_down_button)).check(matches(isDisplayed()));
+    public void testUserCanUpOriginalPost() throws InterruptedException {
+        // Can remove comments when update document function is mocked
+        // TimeUnit.SECONDS.sleep(3);
+        // onData(instanceOf(Post.class)).inAdapterView(withId(R.id.reply_original_post)).atPosition(0).onChildView(withId(R.id.post_up_button)).perform(click());
     }
 
     @Test
-    public void testUserCanUpReply() {
-        // onData(anything()).inAdapterView(withId(R.id.reply_list_view)).onChildView(withId(R.id.post_up_button)).check(matches(isDisplayed()));
-        // onData(anything()).inAdapterView(withId(R.id.reply_list_view)).onChildView(withId(R.id.post_down_button)).check(matches(isDisplayed()));
+    public void testUserCanDownOriginalPost() throws InterruptedException {
+        // Can remove comments when update document function is mocked
+        // TimeUnit.SECONDS.sleep(3);
+        // onData(instanceOf(Post.class)).inAdapterView(withId(R.id.reply_original_post)).atPosition(0).onChildView(withId(R.id.post_down_button)).perform(click());
+    }
+
+    @Test
+    public void testUserCanUpReply() throws InterruptedException {
+        // Can remove comments when update document function is mocked
+        // TimeUnit.SECONDS.sleep(3);
+        // onData(instanceOf(Post.class)).inAdapterView(withId(R.id.reply_list_view)).atPosition(0).onChildView(withId(R.id.post_up_button)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testUserCanDownReply() throws InterruptedException {
+        // Can remove comments when update document function is mocked
+        // TimeUnit.SECONDS.sleep(3);
+        // onData(instanceOf(Post.class)).inAdapterView(withId(R.id.reply_list_view)).atPosition(0).onChildView(withId(R.id.post_down_button)).check(matches(isDisplayed()));
     }
 }
