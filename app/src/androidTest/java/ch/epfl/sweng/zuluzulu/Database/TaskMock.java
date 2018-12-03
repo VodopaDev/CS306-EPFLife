7d3e9ad04612553b41fd54763d97aca3d2cd1f8b
Dahn Y <dahn.youssefi@epfl.ch>
2018-12-03 22:58:06 +0100

    Should work !!!!

--- app/src/androidTest/java/ch/epfl/sweng/zuluzulu/Database/TaskMock.java
+++ app/src/androidTest/java/ch/epfl/sweng/zuluzulu/Database/TaskMock.java
@@ -47,7 +47,6 @@ class TaskMock <T> extends Task<T> {
         @NonNull
         @Override
         public com.google.android.gms.tasks.Task<T> addOnSuccessListener(@NonNull OnSuccessListener<? super T> onSuccessListener) {
-       //     onSuccessListener.onSuccess(null);
             return this;
         }
 
