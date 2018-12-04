869f899b5246e282f6ec8df16042281afd2d64ce
Yann <yann.gabbud@epfl.ch>
2018-12-04 15:29:07 +0100

    #244 add more test

--- app/src/androidTest/java/ch/epfl/sweng/zuluzulu/Fragments/MainFragmentGuestUserTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/zuluzulu/Fragments/MainFragmentGuestUserTest.java
@@ -1,4 +1,16 @@
 package ch.epfl.sweng.zuluzulu.Fragments;
 
-public class MainFragmentGuestUserTest {
+import ch.epfl.sweng.zuluzulu.Database.MockedProxy;
+import ch.epfl.sweng.zuluzulu.Firebase.DatabaseFactory;
+import ch.epfl.sweng.zuluzulu.TestingUtility.TestWithGuestAndFragment;
+
+public class MainFragmentGuestUserTest extends TestWithGuestAndFragment<MainFragment> {
+
+    @Override
+    public void initFragment() {
+        DatabaseFactory.setDependency(new MockedProxy());
+        fragment = MainFragment.newInstance(user);
+    }
+
+    // TODO
 }
