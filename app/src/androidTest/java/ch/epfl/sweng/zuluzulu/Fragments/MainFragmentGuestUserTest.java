f8cfbe8e60ea59c33b647dfcaa3a643c00bc542f
Yann <yann.gabbud@epfl.ch>
2018-12-04 15:32:50 +0100

    #244 add reference to guest layout

--- app/src/androidTest/java/ch/epfl/sweng/zuluzulu/Fragments/MainFragmentGuestUserTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/zuluzulu/Fragments/MainFragmentGuestUserTest.java
@@ -1,8 +1,17 @@
 package ch.epfl.sweng.zuluzulu.Fragments;
 
+import org.junit.Test;
+
 import ch.epfl.sweng.zuluzulu.Database.MockedProxy;
 import ch.epfl.sweng.zuluzulu.Firebase.DatabaseFactory;
+import ch.epfl.sweng.zuluzulu.R;
 import ch.epfl.sweng.zuluzulu.TestingUtility.TestWithGuestAndFragment;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 public class MainFragmentGuestUserTest extends TestWithGuestAndFragment<MainFragment> {
 
@@ -12,5 +21,9 @@ public class MainFragmentGuestUserTest extends TestWithGuestAndFragment<MainFrag
         fragment = MainFragment.newInstance(user);
     }
 
-    // TODO
+    @Test
+    public void elementsArePresent(){
+        onView(withText("welcome")).check(matches(isDisplayed()));
+
+    }
 }
