30891b725be1d847ab4e361395d0d853f384f605
Dahn Y <dahn.youssefi@epfl.ch>
2018-12-04 11:20:56 +0100

    Better tests

--- app/src/androidTest/java/ch/epfl/sweng/zuluzulu/Database/DatabaseFactoryTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/zuluzulu/Database/DatabaseFactoryTest.java
@@ -1,14 +1,21 @@
 package ch.epfl.sweng.zuluzulu.Database;
 
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
 import org.junit.Test;
 
 import ch.epfl.sweng.zuluzulu.Firebase.DatabaseFactory;
 import ch.epfl.sweng.zuluzulu.Firebase.Proxy;
+import ch.epfl.sweng.zuluzulu.MainActivity;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.*;
 
 public class DatabaseFactoryTest {
+    @Rule
+    public final ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
 
     @Test
     public void getDependency() {
