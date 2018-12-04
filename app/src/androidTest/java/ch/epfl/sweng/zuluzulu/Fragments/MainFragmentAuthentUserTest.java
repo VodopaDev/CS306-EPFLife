508d3268477c8470a52adea93b790f84dc8d9e76
Yann <yann.gabbud@epfl.ch>
2018-12-04 15:08:37 +0100

    #244 add test

--- app/src/androidTest/java/ch/epfl/sweng/zuluzulu/Fragments/MainFragmentAuthentUserTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/zuluzulu/Fragments/MainFragmentAuthentUserTest.java
@@ -1,4 +1,42 @@
 package ch.epfl.sweng.zuluzulu.Fragments;
 
-public class MainFragmentAuthentUserTest {
+import org.junit.Before;
+import org.junit.Test;
+
+import ch.epfl.sweng.zuluzulu.Database.MockedProxy;
+import ch.epfl.sweng.zuluzulu.Firebase.DatabaseFactory;
+import ch.epfl.sweng.zuluzulu.R;
+import ch.epfl.sweng.zuluzulu.Structure.Association;
+import ch.epfl.sweng.zuluzulu.TestWithAuthenticatedUser;
+import ch.epfl.sweng.zuluzulu.User.AuthenticatedUser;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+public class MainFragmentAuthentUserTest extends TestWithAuthenticatedUser {
+    SuperFragment fragment;
+
+    @Before
+    public void init() {
+        DatabaseFactory.setDependency(new MockedProxy());
+        AuthenticatedUser user = getUser();
+        user.addFollowedAssociation()
+        fragment = MainFragment.newInstance(user);
+        mActivityRule.getActivity().openFragment(fragment);
+    }
+
+    @Test
+    public void elementsArePresent(){
+        onView(withId(R.id.main_fragment_image)).check(matches(isDisplayed()));
+        onView(withId(R.id.main_fragment_followed_associations_text)).check(matches(isDisplayed()));
+        onView(withId(R.id.main_fragment_followed_events_text)).check(matches(isDisplayed()));
+        onView(withId(R.id.main_fragment_followed_associations_listview)).check(matches(isDisplayed()));
+        onView(withId(R.id.main_fragment_followed_events_listview)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void
 }
