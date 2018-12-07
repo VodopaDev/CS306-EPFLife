76865a873309b7a50c42908ec531af06002fecf8
Dahn Y <dahn.youssefi@epfl.ch>
2018-12-07 11:02:35 +0100

    Event detail test added

--- app/src/androidTest/java/ch/epfl/sweng/zuluzulu/Fragments/EventDetailFragmentTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/zuluzulu/Fragments/EventDetailFragmentTest.java
@@ -22,30 +22,27 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.CoreMatchers.anything;
 
 @RunWith(AndroidJUnit4.class)
-public class EventDetailFragmentTest extends TestWithAuthenticatedAndFragment<EventFragment> {
+public class EventDetailFragmentTest extends TestWithAuthenticatedAndFragment<EventDetailFragment> {
 
     @Override
     public void initFragment() {
         DatabaseFactory.setDependency(new MockedProxy());
-        fragment = EventFragment.newInstance(user);
+        fragment = EventDetailFragment.newInstance(user, Utility.defaultEvent());
     }
 
     @Test
-    public void authenticatedCanOpenAnEvent(){
-        onData(anything()).inAdapterView(ViewMatchers.withId(R.id.event_fragment_listview)).atPosition(0).perform(click());
-        onView(withId(R.id.event_detail_fav)).check(matches(isDisplayed()));
+    public void addFavotite(){
+        onView(withId(R.id.event_detail_fav)).perform(click());
     }
 
    @Test
     public void authenticatedCanOpenTheChatOfAnEvent() {
-        onData(anything()).inAdapterView(withId(R.id.event_fragment_listview)).atPosition(0).perform(click());
         onView(withId(R.id.event_detail_chatRoom)).perform(click());
         Utility.checkFragmentIsOpen(R.id.chat_fragment);
     }
 
     @Test
     public void authenticatedCanOpenTheAssociationOfAnEvent() {
-        onData(anything()).inAdapterView(withId(R.id.event_fragment_listview)).atPosition(0).perform(click());
         onView(withId(R.id.event_detail_but_assos)).perform(click());
         Utility.checkFragmentIsOpen(R.id.association_detail_fragment);
     }
