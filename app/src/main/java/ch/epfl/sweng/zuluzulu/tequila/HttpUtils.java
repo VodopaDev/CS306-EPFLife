514ef2339d691b89270306b8acdfb93d88ff2134
Dahn Y <dahn.youssefi@epfl.ch>
2018-12-16 23:28:33 +0100

    remove println

--- app/src/main/java/ch/epfl/sweng/zuluzulu/tequila/HttpUtils.java
+++ app/src/main/java/ch/epfl/sweng/zuluzulu/tequila/HttpUtils.java
@@ -30,7 +30,6 @@ public final class HttpUtils {
             HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
             InputStream stream = connection.getInputStream();
             String json = new Scanner(stream).useDelimiter("\\A").next();
-            System.out.println(json);
             return new Gson().fromJson(json, classOfT);
         } catch (MalformedURLException e) {
             throw new AssertionError("The URL is malformed!?");
